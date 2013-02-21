c89cfcaeecb8dc54c134ad52dddfd4347f1d3c64
Brian Crain <crainbf@gmail.com>
2013-02-21 08:11:59 +0000

    Big Update. Saving trial values to array

--- script.js
+++ script.js
@@ -10,36 +10,81 @@ var TEXT = new Array(
 	"GREEN"
 );
 
+
+var num_correct = 0;
+var num_trials = 0;
+var trials = new Array();
+
+
 $(document).ready(function(){
 	$('#start').click(start);
 });
 
 function start() {
+	//hide start button
+	$('#start').hide();
+
+	//write hello in random color
 	stimColor = Math.floor(3*Math.random());
 	$('#stimulus').css('color', COLORS[stimColor]);
 	$('#stimulus').html('Hello');
 
-	// $('#red').click(response);
-	// $('#blue').click(response);
+	//increment trial by 1 and write to counter
+	num_trials++;
+	$('#num_trials').html(num_trials);
+
+	//record start time
+	var d = new Date();
+	start_time = d.getTime();
 }
 
 function response(clicked_id) {
 	if (clicked_id === COLORS[stimColor]) {
+		//create trial object
+		var trial ={
+			duration: get_duration(),
+			stimulus_color: COLORS[stimColor],
+			correct: true
+		};
+
+		$('body').data('duration', get_duration())
+
+		//Save the duration to an array
+		trials[num_trials-1] = trial;
+
 		$('#response').html('The answer is correct.');
+		num_correct++;
+
+		$('#score').html(num_correct+' out of '+num_trials);
+		start();
+
 	} else {
+		//create trial object
+		var trial ={
+			duration: get_duration(),
+			stimulus_color: COLORS[stimColor],
+			correct: false
+		};
+		//Save the duration to an array
+		trials[num_trials-1] = get_duration();
+		
 		$('#response').html('The answer is NOT correct.');
+
+		$('#score').html(num_correct+' out of '+num_trials);
+		start();
 	}
 
 }
-// $(document).ready(function(){
-// 	//$('input[type="button"][value="red"]').click(function(){ 
-// 	$('div').mouseenter(function(){
-// 		$('div').hide();
-// 	});
 
+function get_duration(){
+	//gets trial duration
+	var d = new Date();
+	var end_time = d.getTime();
+	trial_duration =  end_time - start_time;
 
+	//prints duration to screen
+	$('#trial_duration').html(trial_duration);
+	return trial_duration
+}
 
-// 	//alert('hello');
-// 	//$('#stimulus').fadeOut(1000);
-// });
 
