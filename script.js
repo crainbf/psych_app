4fc402c22d92eeb6a9d59066a791c650d1841595
Brian Crain <crainbf@gmail.com>
2013-02-21 15:20:14 +0000

    Bring Style with JSLint in agreement

--- script.js
+++ script.js
@@ -1,90 +1,86 @@
-var COLORS = new Array(
-	"red",
-	"blue",
-	"green"
-);
+/*global $, document*/
 
-var TEXT = new Array(
-	"RED",
-	"BLUE",
-	"GREEN"
-);
+var COLORS = ["red", "blue", "green"];
 
+var TEXT = ["RED", "BLUE", "GREEN"];
 
 var num_correct = 0;
 var num_trials = 0;
-var trials = new Array();
-
-
-$(document).ready(function(){
-	$('#start').click(start);
-});
+var trials = [];
+var stimColor, start_time;
 
 function start() {
+	"use strict";
 	//hide start button
 	$('#start').hide();
 
 	//write hello in random color
-	stimColor = Math.floor(3*Math.random());
+	stimColor = Math.floor(3 * Math.random());
 	$('#stimulus').css('color', COLORS[stimColor]);
 	$('#stimulus').html('Hello');
 
 	//increment trial by 1 and write to counter
-	num_trials++;
+	num_trials += 1;
 	$('#num_trials').html(num_trials);
 
 	//record start time
-	var d = new Date();
-	start_time = d.getTime();
+	start_time = Date.now();
 }
 
+$(document).ready(function () {
+	"use strict";
+	$('#start').click(start);
+});
+
+function get_duration() {
+	"use strict";
+	//gets trial duration
+	var trial_duration =  Date.now() - start_time;
+
+	//prints duration to screen
+	$('#trial_duration').html(trial_duration);
+	return trial_duration;
+}
+
+
 function response(clicked_id) {
+	"use strict";
+	var trial = {
+		duration: get_duration(),
+		stimulus_color: COLORS[stimColor]
+	};
+
 	if (clicked_id === COLORS[stimColor]) {
-		//create trial object
-		var trial ={
-			duration: get_duration(),
-			stimulus_color: COLORS[stimColor],
-			correct: true
-		};
+		trial.correct = true;
+		num_correct += 1;
 
-		$('body').data('duration', get_duration())
-
-		//Save the duration to an array
-		trials[num_trials-1] = trial;
+		//Save the trial to the trials array
+		trials[num_trials - 1] = trial;
 
 		$('#response').html('The answer is correct.');
-		num_correct++;
+		$('#score').html(num_correct + ' out of ' + num_trials);
 
-		$('#score').html(num_correct+' out of '+num_trials);
 		start();
 
 	} else {
-		//create trial object
-		var trial ={
-			duration: get_duration(),
-			stimulus_color: COLORS[stimColor],
-			correct: false
-		};
-		//Save the duration to an array
-		trials[num_trials-1] = get_duration();
-		
-		$('#response').html('The answer is NOT correct.');
+		trial.correct = false;
+		//Save the trial to the trials array
+		trials[num_trials - 1] = trial;
 
-		$('#score').html(num_correct+' out of '+num_trials);
+		$('#response').html('The answer is NOT correct.');
+		$('#score').html(num_correct + ' out of ' + num_trials);
+
 		start();
 	}
 
 }
 
-function get_duration(){
-	//gets trial duration
-	var d = new Date();
-	var end_time = d.getTime();
-	trial_duration =  end_time - start_time;
+//Not actually using this function yet. 
+function interrupt() {
+	"use strict";
 
-	//prints duration to screen
-	$('#trial_duration').html(trial_duration);
-	return trial_duration
+	$('#stimulus').html('');
 }
+
 
 
