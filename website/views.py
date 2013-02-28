7adb9bc4e3f142d5f76d07f1f44961e89edce36f
Brian Crain <crainbf@gmail.com>
2013-02-28 16:51:21 +0000

    Adding csrf_exempt decorator

--- website/views.py
+++ website/views.py
@@ -2,17 +2,19 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 import json
 from website.models import TrialResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
     return render(request, 'website/home.html')
 
 
+@csrf_exempt
 def trial(request):
     #We're posting only with AJAX, so if it is not ajax, don't do anything
     if request.is_ajax():
         if request.method == 'POST':
-            #The request.POST variable doesn't work so well whe using JSON,
+            #The request.POST variable doesn't work so well when using JSON,
             #so we'll read the body of the request instead
             data = json.loads(request.body)
             #create the objects we want to insert into the database
