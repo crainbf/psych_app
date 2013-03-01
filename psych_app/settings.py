b66da24ea04817590325a3cb307592b54f4186a3
Brian Crain <crainbf@gmail.com>
2013-03-01 11:22:50 +0000

    Adding South and Requirements Files

--- psych_app/settings.py
+++ psych_app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
