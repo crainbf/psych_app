4953f0b231a978e42a1c6443d9e2e4d9d83e7c32
Brian Crain <crainbf@gmail.com>
2013-03-05 17:39:07 +0000

    Adding participant ID

--- website/models.py
+++ website/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 # Create your models here.
 class TrialResponse(models.Model):
+    participant_id = models.IntegerField()
     stim_color = models.CharField(max_length=16)
     stim_word = models.CharField(max_length=16)
     response_color = models.CharField(max_length=16)
     reaction_time = models.IntegerField()
+
