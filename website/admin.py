2132c08b923c344847d4a930a4f2c85ccb0c6965
Brian Crain <crainbf@gmail.com>
2013-03-14 10:25:25 +0000

    Customizing Admin view

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from website.models import TrialResponse
 
-admin.site.register(TrialResponse)
+
+class TrialResponseAdmin(admin.ModelAdmin):
+    list_display = ('participant_id', 'session_number', 'reaction_time')
+    list_filter = ('participant_id','session_number',)
+    ordering = ('participant_id',)
+
+
+admin.site.register(TrialResponse, TrialResponseAdmin)
--- website/models.py
+++ website/models.py
@@ -4,8 +4,11 @@ from django.db import models
 # Create your models here.
 class TrialResponse(models.Model):
     participant_id = models.IntegerField()
-    stim_color = models.CharField(max_length=16)
-    stim_word = models.CharField(max_length=16)
-    response_color = models.CharField(max_length=16)
+    stim_color = models.CharField(max_length=16, verbose_name='Stimulus Color')
+    stim_word = models.CharField(max_length=16, verbose_name='Stimulus Word')
+    response_color = models.CharField(max_length=16, verbose_name='Response Color')
     reaction_time = models.IntegerField()
     session_number = models.IntegerField()
+
+    def __unicode__(self):
+        return u'Participant %s. Reaction Time: %s' % (self.participant_id, self.reaction_time)
