e10a9b28675264f59736094427793ffa50a875ff
Brian Crain <crainbf@gmail.com>
2013-03-27 13:25:53 +0000

    Extending functional tests - Fixtures not yet loading properly

--- fts/tests.py
+++ fts/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 
 
 class AdminFunctionalTest(LiveServerTestCase):
-    fixtures = ['admin_user.json']
+    fixtures = ['admin_user.json', 'initial_data.json']
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,7 +13,7 @@ class AdminFunctionalTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_log_into_admin_site_is_displaying(self):
+    def test_can_log_into_admin_site_TrialResponse_displaying(self):
         # Mark opens his web browser and goes to the admin page
         self.browser.get(self.live_server_url + '/admin/')
 
@@ -32,3 +32,14 @@ class AdminFunctionalTest(LiveServerTestCase):
         # Can see the Trial Response objects
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Trial responses', body.text)
+
+        # He clicks on the 'Trial responses' link
+        trialresponse_link = self.browser.find_element_by_link_text('Trial responses')
+        trialresponse_link.click()
+
+        # He sees the the tabs for 'Participant id', 'Session number' and 'Reaction time'
+        body = self.browser.find_element_by_tag_name('body')
+        self.browser.implicitly_wait(3)
+        self.assertIn('Participant id', body.text)
+        self.assertIn('Session number', body.text)
+        self.assertIn('Reaction time', body.text)
